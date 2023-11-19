9913a2742e81c5240a1695f192d759f04ab37ad0
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-19 08:32:19 +0200

    add employee rest controller

---
+++ src/main/java/com/mohamed/springboot/rest/EmployeeRestController.java
@@ -0,0 +1,28 @@
+package com.mohamed.springboot.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mohamed.springboot.entity.Employee;
+import com.mohamed.springboot.service.EmployeeService;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+	private EmployeeService service;
+	
+	@Autowired
+	public EmployeeRestController(EmployeeService service) {
+		this.service = service;
+	}
+	
+	@GetMapping("/employee")
+	public List<Employee> findAll(){
+		return service.findAll();
+	}
+}
