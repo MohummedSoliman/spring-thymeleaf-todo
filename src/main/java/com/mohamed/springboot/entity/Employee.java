d1c6399c7c2c326408182286e1ba544a3153bfde
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-19 08:31:56 +0200

    add employee entity class

---
+++ src/main/java/com/mohamed/springboot/entity/Employee.java
@@ -0,0 +1,74 @@
+package com.mohamed.springboot.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email")
+	private String name;
+
+	public Employee() {
+
+	}
+
+	public Employee(String firstName, String lastName, String name) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", name=" + name + "]";
+	}
+
+}
