c4e60350f9c59e710bad237b034f258cbb6128ec
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-19 16:09:10 +0200

    add endpoint for update, delete and return specific employee

--- src/main/java/com/mohamed/springboot/controller/EmployeeController.java
+++ src/main/java/com/mohamed/springboot/controller/EmployeeController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.mohamed.springboot.entity.Employee;
 import com.mohamed.springboot.service.EmployeeService;
@@ -28,4 +32,49 @@ public class EmployeeController {
 		theModel.addAttribute("employees", employees);
 		return "list-employees";
 	}
+	
+	@GetMapping("/showForm")
+	public String showAddEmployeeForm(Model theModel) {
+		Employee theEmployee = new Employee();
+		
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
+		service.save(theEmployee);
+		return "redirect:/employees/list";
+	}
+	
+	@GetMapping("showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int id,
+			Model theModel) {
+		Employee theEmployee = service.findById(id);
+		
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
+	}
+	
+	@GetMapping("deleteEmployee")
+	public String deleteEmployee(@RequestParam("employeeId") int theId) {
+		service.deleteById(theId);
+		return "redirect:/employees/list";
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
