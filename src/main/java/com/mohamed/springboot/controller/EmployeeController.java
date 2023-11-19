ed235f78e487bff9509cf9248a9bd70463604593
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-19 13:50:06 +0200

    add html file for display list o employee

--- src/main/java/com/mohamed/springboot/controller/EmployeeController.java
+++ src/main/java/com/mohamed/springboot/controller/EmployeeController.java
@@ -1,5 +1,31 @@
 package com.mohamed.springboot.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mohamed.springboot.entity.Employee;
+import com.mohamed.springboot.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employees")
 public class EmployeeController {
 
+	private EmployeeService service;
+	
+	@Autowired
+	public EmployeeController(EmployeeService service) {
+		this.service = service;
+	}
+	
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		List<Employee> employees = service.findAll();
+		theModel.addAttribute("employees", employees);
+		return "list-employees";
+	}
 }
