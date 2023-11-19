2f976cb1ea2ad762ef629ccb979a462b41e9c5ac
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-11-19 08:03:27 +0200

    initial commit

---
+++ src/main/java/com/mohamed/springboot/ThymeleaTodoApplication.java
@@ -0,0 +1,13 @@
+package com.mohamed.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleaTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleaTodoApplication.class, args);
+	}
+
+}
